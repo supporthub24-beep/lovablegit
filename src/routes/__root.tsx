ad979c5fa205ba4c95f98f0442a12213ad223392
supporthub24-beep <supporthub24@gmail.com>
2026-09-12 23:21:47 +0300

    Restore global stylesheet connection

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,6 +1,6 @@
+import "@/styles.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import {
-import "@/styles.css";
   Outlet,
   Link,
   createRootRouteWithContext,
